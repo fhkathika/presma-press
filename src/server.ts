930c99c149933cfe90578784614fbe0f929b918b
fhkathika <fhkathika057@gmail>
2026-06-27 16:39:26 +0600

    set up

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import config from "./config";
+// import "dotenv/config";
+const PORT = config.port;
+async function main() {
+    try {
+        // await prisma.$connect()
+        console.log("Connected Database")
+        app.listen(PORT, () => {
+            console.log(`Server is running on gg port${PORT}`);
+        });
+    }
+    catch (error) {
+        console.log("Error starting the Server", error);
+        // await prisma.$disconnect()
+        process.exit(1);
+    }
+}
+main();
+//# sourceMappingURL=server.js.map
