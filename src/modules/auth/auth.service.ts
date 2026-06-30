f63d2c39508431844b1c67706473dd17fd44b0f2
fhkathika <fhkathika057@gmail>
2026-06-30 16:42:47 +0600

    working on authorization

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
-import { ILoginUser } from "./auth.interface"
-
+import { ILoginUser } from "./auth.interface";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
 const loginUser=async (payload:ILoginUser)=>{
 const {email,password}=payload;
 const user=await prisma.user.findUniqueOrThrow({
@@ -11,7 +12,23 @@ const isPasswordMatched=await bcrypt.compare(password,user.password)
 if(!isPasswordMatched){
     throw new Error("Password not matched")
 }
-return user
+const jwtPayload={
+id:user.id,
+email:user.email,
+role:user.role,
+name:user.name
+
+}
+const accessToken=jwt.sign(jwtPayload,config.jwt_access_secret,{
+    expiresIn:config.jwt_access_expires_in
+} as SignOptions
+)
+const refreshToken=jwt.sign(jwtPayload,config.jwt_refresh_secret,{
+    expiresIn:config.jwt_refresh_expires_in
+}  as SignOptions
+
+)
+return {accessToken,refreshToken}
 }
 export const authService={
     loginUser
